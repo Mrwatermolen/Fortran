448da0448f969ed4b0e09a2865cca0109cff0443
Franzero <mr.smallwatermolen@gmail.com>
2022-01-24 14:39:47 +0800

    fix: 修改了一维FDTD的边界计算

--- FDTD/one_dimensional_FDTD_radiation_field.f90
+++ FDTD/one_dimensional_FDTD_radiation_field.f90
@@ -1,26 +1,28 @@
 program one_dimensional_FDTD_radiation_field
     ! 平面电流源的辐射场分布，边界采用Mur一阶近似吸收边界
-    ! 场源位于z=250处，为xoy平面，源为一时谐源：cos(2*PI*3*1e8*t)，区域为真空
-    real Mu0, EP0, c, deltaZ, deltaT, ca, cb, cp, cq, aplha
+    ! 场源位于z=250处，为xoy平面，源为一时谐源：cos(2*pi*3*1e8*t)，区域为真空
+    real mu_0, epsilon_0, pi, c, delta_z, delta_t, ca, cb, cp, cq, alpha
     real Ex(0:500,0:500), Hy(0:500,0:500) !(z,t)
-    integer step, sourceZ, t, z
+    integer step, source_z, t, z
     external current_source
 
-    Mu0 = 4 * PI * 1.0e-7
-    EP0 = 8.85 * 1.0e-12
+    pi = 3.14
+    mu_0 = 4 * pi * 1.0e-7
+    epsilon_0 = 8.85 * 1.0e-12
     c = 3.0e8
-    deltaZ = 1.0 / 40.0
-    deltaT = deltaZ / (2 * c)
+    delta_z = 1.0 / 40.0
+    delta_t = delta_z / (2 * c)
     step = 500
-    sourceZ = 250
+    source_z = 250
     ca = 1.0
-    cb = (deltaT) / (EP0 * deltaZ)
+    cb = (delta_t) / (epsilon_0 * delta_z)
     cp = 1.0
-    cq = (deltaT) / (Mu0 * deltaZ)
-    aplha = (c * deltaT - deltaZ) / (c * deltaT + deltaZ)
+    cq = (delta_t) / (mu_0 * delta_z)
+    alpha = (c * delta_t - delta_z) / (c * delta_t + delta_z)
     ! open(1,file='electric_field.dat')
     ! open(2,file='magnetic_field.dat')
-    open(3,file='electric_field_while_deltaT_200.dat')
+    open(3,file='electric_field_while_delta_t_200.dat')
+    open(4,file='magnetric_field_while_delta_t_400.dat')
 
     ! 赋初值
     do i = 0, step
@@ -33,24 +35,28 @@ program one_dimensional_FDTD_radiation_field
     ! 主体
     do t = 1, step
         ! 先计算Ex
-        do z = 1, step - 1
-            if ( z == sourceZ ) then
+        do z = 0, step
+            if ( z == source_z ) then
                 ! 处理元胞包含电流源时
                 Ex(z,t) = ca * Ex(z, t - 1) - cb * ( Hy(z, t - 1) - Hy(z - 1, t - 1) ) &
-                - cb * current_source( t * deltaT )
+                - cb * current_source( (t + 0.5) * delta_t )
+            else if ( z == 0 ) then
+                ! 处理边界 Mur一阶近似吸收左边界
+                Ex(0, t) = Ex(1, t - 1) + alpha * ( Ex(1, t) - Ex(0, t - 1) )
+            else if ( z == step ) then
+                ! 处理边界 Mur一阶近似吸收右边界
+                Ex(step, t) = Ex(step - 1, t - 1) + alpha * ( Ex(step - 1, t) - Ex(step, t - 1) )
             else
                 Ex(z,t) = ca * Ex(z, t - 1) - cb * ( Hy(z, t - 1) - Hy(z - 1, t - 1) )
             end if
         end do
-        do z = 1, step - 1
+        ! Hy分量 z = 0时 Hy(z + 1/2)的计算不涉及截断边界以外的计算， Hy仅仅需计算到第step个元胞  ，因为第step + 1元胞已经超出了截断边界。Hy的划分为z+1/2
+        do z = 0, step - 1
             Hy(z,t) = cp * Hy(z, t - 1) - cq * ( Ex(z + 1, t) - Ex(z, t) )
         end do
 
-        ! 处理边界 Mur一阶近似吸收边界
-        Ex(0, t) = Ex(1, t - 1) + aplha * ( Ex(1, t) - Ex(0, t - 1) )
-        Hy(0, t) = Hy(1, t - 1) + aplha * ( Hy(1, t) - Hy(0, t - 1) )
-        Ex(step, t) = Ex(step - 1, t - 1) + aplha * ( Ex(step - 1, t) - Ex(step, t - 1) )
-        Hy(step, t) = Hy(step - 1, t - 1) + aplha * ( Hy(step - 1, t) - Hy(step, t - 1) )
+        !Hy(0, t) = Hy(1, t - 1) + alpha * ( Hy(1, t) - Hy(0, t - 1) )
+        !Hy(step, t) = Hy(step - 1, t - 1) + alpha * ( Hy(step - 1, t) - Hy(step, t - 1) )
     end do
     
     ! 数据整理
@@ -62,16 +68,17 @@ program one_dimensional_FDTD_radiation_field
     !         write(2,*)z, Hy(z,t)
     !     end do
     ! end do
-    ! t=200*deltaT时的Ex数据
+    ! t=200*delta_t时的Ex数据
     do z = 0, step
         write(3,*)z, Ex(z,200)
+        write(4,*)z, Hy(z,400)
     end do
 end program one_dimensional_FDTD_radiation_field
 
 !电流源
 function current_source(t)
-    real PI, current_source, t
-    PI = 3.14
-    current_source = cos( 2 * PI * 3 * 1e8 *t )
+    real pi, current_source, t
+    pi = 3.14
+    current_source = cos( 2 * pi * 3 * 1e8 *t )
     return
 end
